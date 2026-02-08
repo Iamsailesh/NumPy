4a301dcb098da63fd6b481bc334b067560f74f99
Iamsailesh <nepalsailesh10@gmail.com>
2026-02-08 23:07:21 +0000

    done some vactorized math as well as comparision operators

--- file3.py
+++ file3.py
@@ -17,3 +17,36 @@ print(array[:, 1:3])
 #selecting only column
 
 print(array[:, 1])
+
+print("Arthemetic operation in Array")
+
+newArray = np.array([12,4,6,8])
+print(newArray *2)
+print(newArray -7)
+print(newArray +3)
+print(newArray / 2)
+print(np.sqrt(newArray))
+#can use np.round to round and np.floor to round down as well as np.ceil to round up
+
+print("Vactorized math functions")
+
+radii = np.array([1,2,3])
+radii2 = np.array([4,5,6])
+
+print(np.pi * radii **2)
+print(radii + radii2)
+print(radii - radii2)
+print(radii * radii2)
+print(radii ** radii2)
+
+print("Comparasion operators")
+
+scores = np.array([91,55,100,73,82,64,42])
+
+print(scores == 100)
+print(scores < 50)
+print(scores > 50)
+
+scores[scores < 50] = 0
+print(scores)
+
